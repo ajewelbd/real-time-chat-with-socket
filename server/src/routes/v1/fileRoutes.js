4632ce9c7e600e6bcf2e342b67bd3dea970be782
Md. Ashraful Alam <imobilzbd@gmail.com>
2024-10-15 22:10:12 +0600

    docker related file updated

--- server/src/routes/v1/fileRoutes.js
+++ server/src/routes/v1/fileRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
+import authMiddleware from "../../middleware/authMiddleware.js";
 import FileStorage, { saveFile } from "../../controllers/v1/FileController.js";
 
 const router = express.Router();
 
-router.post("/upload", FileStorage.single('file'), saveFile);
+router.post("/upload", authMiddleware, FileStorage.single('file'), saveFile);
 
 export default router;
